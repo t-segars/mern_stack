6a995bec055cf9dd63a8ee9dea0f38016cf654d9
Tim <98896176+twistedstached@users.noreply.github.com>
2022-07-12 13:26:38 -0700

    pokemon api using axios to "fetch:

---
+++ pokemonapi/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Form from "./components/Form";
+
+function App() {
+    return (
+        <fieldset>
+            <legend>App.js</legend>
+            <Form />
+        </fieldset>
+    );
+}
+
+export default App;
---
+++ pokemonapi/src/components/Form.jsx
@@ -0,0 +1,49 @@
+
+import '../App.js'
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+const Example = (props) => {
+  // ----- VANILLA STATE ---------------//
+  // const [pokemon, setPokemon] = useState([]);
+
+  const [pokemon, setPokemon] = useState();
+
+  const pokemonHandler = () => {
+    // --------------VANILLA JS METHOD (FETCH)-----------//
+    // fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
+      // SUCCESSFUL RESPONSE
+      // .then(response => response.json())
+      // .then(response => setPokemon(response.results))
+      // UNSUCCESSFUL RESPONSE
+      // .catch(err => console.error("ERROR", err))
+
+    // -------------AXIOS----------------------------//
+    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=807")
+      // SUCCESSFUL RESPONSE
+      .then(response => {
+        console.log("SUCCESS", response.data) 
+        setPokemon(response.data.results)
+      })
+      // UNSUCCESSFUL RESPONSE
+      .catch(err => console.error("ERROR", err)) 
+  };
+
+  return (
+    <fieldset style={{textAlign:'center'}}>
+      <div>
+        
+        <button onClick={pokemonHandler}>Fetch Pokemon</button>
+        {
+        // ----- VANILLA FUNCTION TO DISPLAY DATA ----------------------//
+       
+        pokemon &&
+        pokemon.length > 0 && pokemon.map((poke, index)=>{
+          return (<li key={index}>{index}: {poke.name}{poke.font_default}</li>)})
+        }
+      </div>
+    </fieldset>
+  );
+}
+export default Example;
